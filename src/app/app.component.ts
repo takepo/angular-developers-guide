c694e399f1737cc8b8fb14cf1a37258dc04a8793
TakeshimaNahoko <takeshima.nahoko@lancers.co.jp>
2019-12-31 16:42:44 +0900

    feat: 2-2 イベントバインディング

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <p>1 + 1 = {{1 + 1}}</p>
-  <p>{{a}} * {{b}} = {{a * b}}</p>
-  <p>{{c}} + {{d}} = {{[c,d].join(' ')}}</p>
-  <p>eが{{e}}であれば、{{c}} => {{(e)? c:d}}</p>
+  <button (click)="clickButton($event)">ボタン</button>
   `,
 })
 export class AppComponent {
-  a = 2;
-  b = 3;
-  c = 'Hello';
-  d = 'Angular!';
-  e = true;
+  clickButton($event) {
+    console.log($event);
+  }
 }
