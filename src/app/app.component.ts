520fcb4784130bb9829b6d6fc60d9357cb69ebda
TakeshimaNahoko <takeshima.nahoko@lancers.co.jp>
2019-12-31 16:53:14 +0900

    feat: 2-4 attributeBuinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <button [disabled]="isDisabled">ボタン</button>
-  <img [src]="imagePath" />
+  <table>
+    <tr>
+      <td [attr.colspan]="colspanSize">2セル分を1セルとする</td>
+    </tr>
+    <tr>
+      <td>1セル目</td>
+      <td>2セル目</td>
+    </tr>
+  </table>
   `,
+  styles: [
+    'table, tr{ border: solid 1px #333 }'
+  ]
 })
 export class AppComponent {
-  isDisabled = true;
-  imagePath = 'https://angular.io/assets/images/logos/angular/angular.svg';
+  colspanSize = 2;
 }
