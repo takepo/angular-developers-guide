aaec9fcafe8f898125fdbd54ef428cca2012dc76
TakeshimaNahoko <takeshima.nahoko@lancers.co.jp>
2019-12-31 16:37:54 +0900

    feat: 2-1 expressions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `template`,
+  template: `
+  <p>1 + 1 = {{1 + 1}}</p>
+  <p>{{a}} * {{b}} = {{a * b}}</p>
+  <p>{{c}} + {{d}} = {{[c,d].join(' ')}}</p>
+  <p>eが{{e}}であれば、{{c}} => {{(e)? c:d}}</p>
+  `,
 })
 export class AppComponent {
+  a = 2;
+  b = 3;
+  c = 'Hello';
+  d = 'Angular!';
+  e = true;
 }
