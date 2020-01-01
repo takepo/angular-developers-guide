2510955233ba917812cbe46128c71b969ba1ff80
TakeshimaNahoko <takeshima.nahoko@lancers.co.jp>
2020-01-01 22:10:29 +0900

    chore: ng g component simple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SimpleComponent } from './simple/simple.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleComponent
   ],
   imports: [
     BrowserModule
