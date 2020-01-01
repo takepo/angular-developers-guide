e94836d233e04331737909db8245dfa43c4cdc44
TakeshimaNahoko <takeshima.nahoko@lancers.co.jp>
2020-01-02 00:54:46 +0900

    feat: 2-20 ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  style1 = {
-    'font-weight': 'boid',
-    'text-decoration': 'line-through',
-  }
+  directives = [
+    'hoge',
+    'fuga',
+  ]
 }
